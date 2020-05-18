556aa708e84becab73ff2566928ab74e1ff16683
TheFightingCatfish <seekingblues@gmail.com>
2020-05-18 17:37:01 +0800

    Fix credentials

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,8 @@ import App from "./App.vue";
 import VueGoogleApi from "vue-google-api";
 
 const config = {
-  apiKey: "AIzaSyDt2VxRLzdi8qL2Tl_SVTlOV5XVhTvTt94",
-  clientId:
-    "954999908134-m0bcfcjvukvlbh6hosk4uj8mhqfm7p3o.apps.googleusercontent.com",
+  apiKey: process.env.VUE_APP_API_KEY,
+  clientId: process.env.VUE_APP_CLIENT_ID,
   scope: "https://www.googleapis.com/auth/spreadsheets",
   discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
 };
